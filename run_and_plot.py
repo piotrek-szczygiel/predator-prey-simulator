2503baa47d1925fe563c082b2778385e003eda5b
Piotr Szczygieł <szczygiel.p@gmail.com>
2021-06-05 12:43:02 +0200

    Add replotting only best seeds to run_and_plot.py

--- run_and_plot.py
+++ run_and_plot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import pathlib
 import multiprocessing
 import shutil
 import subprocess
@@ -10,27 +11,34 @@ import functools
 import matplotlib.pyplot as plt
 import pandas as pd
 
-plt.style.use("seaborn")
 
-
-def plot(file):
+def plot(file, min_ticks=0):
     with open(file, "r") as f:
         seed = f.readline().strip()
         df = pd.read_csv(f)
 
-    x = range(df.shape[0])
+    ticks = df.shape[0]
+    if ticks < min_ticks:
+        return None
 
-    plt.figure()
+    x = range(ticks)
+
+    if ticks > 25000:
+        figsize = (6.4 * ticks / 25000, 4.8)
+    else:
+        figsize = (6.4, 4.8)
+
+    plt.figure(figsize=figsize)
     plt.title(f"Seed: {seed}", fontweight="bold")
-    plt.plot(x, df["chicken"], label="chicken", c="green")
-    plt.plot(x, df["wolf"], label="wolf", c="red")
+    plt.plot(x, df["chicken"], label="prey", c="green")
+    plt.plot(x, df["wolf"], label="predator", c="red")
 
     plt.xlabel("tick", fontweight="bold")
     plt.ylabel("population", fontweight="bold")
 
     plt.legend()
 
-    outfile = f"png/{seed}.png"
+    outfile = f"png/{ticks}_{seed}.png"
     plt.savefig(outfile, dpi=300)
     plt.clf()
     return outfile
@@ -43,11 +51,35 @@ def run(sim_exe, ticks, file):
 
 
 def main():
+    if len(sys.argv) == 3 and sys.argv[1] == "choose":
+        min_ticks = int(sys.argv[2])
+        shutil.rmtree("png", ignore_errors=True)
+        os.mkdir("png")
+
+        csv = []
+        for (directory, _, files) in os.walk("csv"):
+            for file in files:
+                path = pathlib.Path(os.path.join(directory, file))
+                if path.suffix == ".csv":
+                    csv.append(path)
+
+        plot_at_least = functools.partial(plot, min_ticks=min_ticks)
+        with multiprocessing.Pool(processes=32) as pool:
+            for i, png in enumerate(pool.imap_unordered(plot_at_least, csv)):
+                if png:
+                    print(f"[{i+1}/{len(csv)}] {png} replotted")
+
+        return
+
     if len(sys.argv) not in [4, 5]:
         print(
             f"usage: {sys.argv[0]} <sim executable> <ticks> <iterations> [processes]",
             file=sys.stderr,
         )
+        print(
+            "\nAfter running this script you can later run this command to replot only the best seeds:"
+        )
+        print(f"  {sys.argv[0]} choose <min_ticks>")
         sys.exit(1)
 
     sim_exe = sys.argv[1]
